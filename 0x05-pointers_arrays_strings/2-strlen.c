b296f7177e8b27408985c1827e45675384b917d2
AhmedSidky <ahmed.hamed.sidky@gmail.com>
2023-09-19 00:18:15 +0300

    yarb

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s:input
+ * return:strlen
+ */
+int _strlen(char *s)
+{
+	int i;
+	for(i = 0,i <(*s),i++)
+	{
+	*s = i;
+	}
+}
