f9458c6b2fedec4a71941d385872d4c01267e8c5
AhmedSidky <ahmed.hamed.sidky@gmail.com>
2023-10-26 00:21:22 +0300

    yarb

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print file name
+ * @argc: count of the arguments supplied to the program and the
+ * @argv: an array of pointers to the strings
+ * Return: Always 0
+*/
+int main(int argc, char const *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
